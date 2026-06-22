f1b219f2fcd8b971216e1542290ac0b845b575f3
Dilhara <chamudithadilhara985@gmail.com>
2026-06-22 12:55:50 +0530

    Refactor Vite configuration for clarity and ensure base path is set correctly

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+
+  // IMPORTANT: change this to your repo name
+  base: '/Portfolio-DilharaC/'
+})
